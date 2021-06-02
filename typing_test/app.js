cad38f4f10eeda942ac47ba984ae7af4ea3dd674
emmanuelokwara <okwara.ec@gmail.com>
2021-06-03 01:13:19 +0200

    changes

--- typing_test/app.js
+++ typing_test/app.js
@@ -1,82 +1,36 @@
-let word = `You can use it for many word games: to create or to solve crosswords, arrowords (crosswords with arrows), word puzzles, to play Scrabble, Words With Friends, hangman, the longest word, and for creative writing: rhymes search for poetry, and words that satisfy constraints from the Ouvroir de Potentielle (OuLiPo: workshop of potential litterature) such as lipograms,k`;
-
-class stringWorkspace{
-    constructor(){
-        this.new_string = word;
-    };S
-    stringarray(){
-        return (this.new_string.split('           '));
+let word = `You can use it for many word games: to create or to solve crosswords, arrowords (crosswords with arrows), word puzzles, to play Scrabble, Words With Friends, hangman, the longest word, and for creative writing: rhymes search for poetry, and words that satisfy constraints from the Ouvroir de Potentielle (OuLiPo: workshop of potential litterature) such as lipograms,k`.toLocaleLowerCase();
+class stringWorkspace {
+    constructor(words) {
+        this.words = words.split('');
+        this.currchar = 0;
+        this.wordcontainer = document.querySelector('.cnt');
     }
-}
-
-class insertWorkspace extends stringWorkspace{
-    constructor(){
-        super();
-        this.body = document.querySelector(".cnt");
-        this.body.innerHTML = '';
-        this.stringarray = this.stringarray();
-    }
-    addToBody(){ 
-        this.stringarray.forEach(el=>{
-            el+=' ';
-            this.body.innerHTML += `<span class="inwrd">${el.split('').map(ele=> `<span class="characters">${ele}</span>`).join("")}</span>`;
-        })
-    
-}
-}
-
-class clickingFunctions {
-    constructor() {
-    this.wordcnt = document.querySelector('.cnt').children;
-    this.wordcn = document.querySelector('.cnt');
-    this.currwrd = 0;
-    this.currchar = 0;
-    this.translate = 30;
-    }
-
-    keycontrolling() {
-        document.body.addEventListener('keypress',(e) => {
-            console.log(e.keyCode);
-            if (e.key === this.wordcnt[this.currwrd].children[this.currchar].textContent) {
-
-                // add the changing of the color and the indicator
-                this.wordcnt[this.currwrd].children[this.currchar].classList.add('right', "gray");
-
-                // This is for the removing the indicator that show the current character
-                if(this.currchar > 0){
-                this.wordcnt[this.currwrd].children[this.currchar].previousElementSibling.classList.remove('right');
-                }
-                this.currchar++;
-
-                // this is for the changing to the next word
-                if((this.currchar === this.wordcnt[this.currwrd].children.length)){
-                    this.currwrd ++;
-                    this.currchar = 0;
-                }
-                //this removes the pointer from the last character of the last word
-                if (this.currwrd > 0 && this.currchar == 0){
-                    this.wordcnt[this.currwrd-1].lastElementChild.classList.remove('right');
-                }
-
-                // this moves the words to the left side.
-                this.wordcn.style.cssText += `transform: translateX(${-this.translate}px);`;
-                this.translate+= 27;
-
-            }
-            else if (e.key != this.wordcnt[this.currwrd].children[this.currchar].textContent)
-            {
-                this.wordcnt[this.currwrd].children[this.currchar].className += 'wrong';
-                this.wordcnt[this.currwrd].children[this.currchar].before(e.key)
-                
-            }
-            
+    addwords() {
+        // cleaning the words
+        this.words.forEach((el) => {
+            this.wordcontainer.innerHTML += `<span class="inwrd">${el}</span>`;
         })
     }
-
+    cursors(num) {
+        this.wordcontainer.children[num].classList.add('active');
+        this.remove = this.currchar > 0 ? this.wordcontainer.children[num - 1].classList.remove('active') : false;
+        return this.wordcontainer.children[num];
+    };
+    movecursor() {
+        document.onkeypress = (el) => {
+            if(el.key == this.cursors(this.currchar).textContent){
+            this.active = el.key == this.wordcontainer.children[this.currchar].textContent ? this.currchar++ :false;
+            document.querySelector('.cnt').style.transform += `translateX(-${this.cursors(this.currchar).getBoundingClientRect().width+3}px)`;
+        }
+        else{
+            this.cursors(this.currchar).outerHTML = `<span class="inerr">${el.key}</span><span class="inwrd">${this.cursors(this.currchar).innerHTML}</span>`;
+            this.currchar +=1;
+            this.cursors(this.currchar);
+        }
+        }
+    }
 }
-
-let string = new insertWorkspace().addToBody();
-let clicks = new clickingFunctions().keycontrolling();
-string;
-clicks;
-
+let workspace = new stringWorkspace(word);
+workspace.addwords();
+workspace.cursors(0);
+workspace.movecursor();
